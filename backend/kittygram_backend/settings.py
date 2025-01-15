6407e9ba3e6f803ebdd8cf1943667b1fb7ead57b
Владислав Наумец <klinicpanik@yandex.ru>
2025-01-15 20:07:28 +0300

    bruh

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.175.138','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['89.169.175.138', 'mykitty.sytes.net', 'localhost','127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
